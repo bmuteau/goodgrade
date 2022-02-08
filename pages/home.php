d9622d74449a41f8d7f6d2f755ed492dc6a021f6
Brandon Muteau <muteaub@gmail.com>
2022-02-08 17:00:15 +0100

    header template

---
+++ pages/home.php
@@ -0,0 +1,18 @@
+<?php
+include '../template/header.php';
+
+
+?>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias aperiam nobis a nihil obcaecati neque possimus in culpa doloremque suscipit eveniet corrupti perspiciatis inventore ab, distinctio molestiae quaerat, consequatur impedit facilis? Perferendis, vitae exercitationem! Deserunt, et quibusdam? Explicabo beatae aperiam iure, tempora commodi quam culpa saepe sed sunt. Qui, cum.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, minima maxime! Voluptatem veniam quibusdam, ducimus vitae, dolor cumque autem, perferendis corrupti laboriosam necessitatibus a id dignissimos accusantium dolorum ut quasi?
+    Quos in nemo aperiam ad quis cupiditate possimus deserunt? Consequatur qui totam commodi laudantium natus delectus unde quasi perferendis tenetur consequuntur doloribus velit labore ipsum provident mollitia tempora, quis quisquam?
+    Veritatis eaque hic culpa. Maiores temporibus, iure quidem atque autem illo, voluptate recusandae quos sapiente mollitia placeat velit, tempore perferendis excepturi sit at beatae dolores soluta! Sed dolores ex beatae.
+    Perferendis fugiat, officia blanditiis maxime a voluptatum, error explicabo cum, quaerat laudantium totam! Numquam beatae eius itaque, aperiam porro vero deserunt tenetur, qui consequatur nesciunt officiis cum autem, voluptate quasi.
+    Id accusantium nihil suscipit sunt illum quis aut vitae, hic eaque soluta fuga cupiditate numquam aperiam sint sit, dolorem nisi necessitatibus obcaecati, dicta nam? Vitae corporis totam consectetur numquam magni.
+    Voluptas quod praesentium placeat tempora sapiente nemo explicabo enim fugit voluptatibus voluptatem, laborum facere, veniam nobis expedita et cum pariatur ratione? Nobis similique accusamus modi praesentium hic repellendus, minus sunt!
+    Dolorem rem dolore nulla blanditiis provident eum nam illo quibusdam unde quo, sapiente praesentium voluptatem perspiciatis exercitationem, doloribus doloremque eligendi architecto voluptatum tempore. Corrupti enim facilis officiis autem, ipsum molestiae.
+    Voluptates ipsa ea, explicabo cupiditate neque harum id sapiente beatae provident qui nihil accusamus, similique aliquam vitae doloribus facere saepe. Maxime, dolores illo! Hic quibusdam recusandae natus! Ipsa, nihil minus.
+    Quia itaque possimus nihil exercitationem illum quibusdam, placeat ex ratione animi ab aliquam ut cupiditate mollitia impedit, necessitatibus nesciunt! Iure illo illum beatae tempore animi quaerat similique, accusantium dolorum fuga!
+    Velit expedita a enim. Quaerat necessitatibus repudiandae error commodi, assumenda accusantium ipsam maiores sapiente tenetur delectus ratione accusamus eos nesciunt, esse cum neque minima exercitationem unde cumque architecto temporibus autem.
+</p>
---
+++ template/header.php
@@ -0,0 +1,41 @@
+<link href="../styles/header.css" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap" rel="stylesheet">
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" crossorigin="anonymous"></script>
+
+
+<section class="header">
+    <div class="left"><img src="../src/Logo1.png" class="logo"></div>
+    <div class="right"><a href="#">Connexion</a><br>
+        <a href="#">Inscription</a>
+    </div>
+</section>
+
+<div class="menu-bg"></div>
+<div class="menu-burger">☰</div>
+<div class="menu-items">
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <div> <a href="#">• Accueil </a> </div>
+    <div><a href="#">• Comment ça marche ? </a></div>
+    <div><a href="#">• FAQ </a></div>
+    <br />
+
+    <div> <a href="#">• Nous contacter</a></div>
+    <div><a href="#">• Mentions légale </a></div>
+    <div><a href="#">• Politique de confidentialité</a></div>
+    <br />
+
+
+    <div><a href="connexionregister">• Se déconnecter</a></div>
+</div>
+
+
+<script src="../script/navbar.js"></script>
