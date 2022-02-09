3ccb29c60fa7c7eacc47b26c70715ee208fc217c
Brandon Muteau <muteaub@gmail.com>
2022-02-09 20:01:36 +0100

    home page finish

--- pages/home.php
+++ pages/home.php
@@ -10,17 +10,59 @@ include '../template/header.php';
 <br>
 <br>
 
-
 <form action="#" method="get">
-    <input class="search" type="search" name="terme">
+    <input class="search" style="font-size: 250%; text-align:center;" type="search" name="terme" value="Rechercher un établissement">
     <img src="../src/iconfinder_search_2561381.svg" class="submit">
 </form>
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, eum magnam. Esse veritatis facere explicabo incidunt temporibus ratione et similique perferendis cumque sed nemo voluptate eos, nobis officiis suscipit voluptatem?
-    Facere dolorum porro quibusdam, reiciendis in, ratione quo provident enim nulla suscipit exercitationem voluptates accusamus quae aut voluptatibus illum, laborum eum ipsa officia. Nam amet fugit, cupiditate molestiae dolore vel.
-    Fuga quod repellat officia fugiat temporibus, quasi consequuntur hic iure accusamus, dicta asperiores minus consectetur. Omnis laborum adipisci illo magni fugiat, molestiae recusandae porro nemo ipsa debitis temporibus odio quo?
-    Dolorem quae reprehenderit porro ipsam laudantium mollitia unde consequatur ad vel commodi assumenda, sint, hic consequuntur tempore ratione a pariatur magni. Quisquam, explicabo architecto sapiente deserunt voluptas ullam necessitatibus deleniti.
-    Exercitationem tenetur beatae dolor tempore et deserunt excepturi accusantium sequi delectus quia culpa numquam recusandae eos, minus nihil at alias? Tenetur officiis molestiae quo provident rem hic eligendi necessitatibus ea.</p>
+
+<h1 class="title">NOS RECOMMANDATIONS </h1>
+
+
+<div class="c">
+
+    <input type="radio" name="a" id="cr-1" checked>
+    <label for="cr-1" style="--hue: 32"></label>
+    <div class="ci" style="--z: 4">
+        <h2 class="ch" style="--h: 32; --s: 80%; --l: 90%">$Variable Name | $Variable note</h2>
+        <img class="img" src="../src/images.jpg" alt="resto1 ">
+    </div>
+
+    <input type="radio" name="a" id="cr-2">
+    <label for="cr-2" style="--hue: 82"></label>
+    <div class="ci" style="--z: 3">
+        <h2 class="ch" style="--h: 82; --s: 80%; --l: 90%">$Variable Name | $Variable note</h2>
+        <img class="img" src="../src/téléchargement.jpg" alt="resto2 ">
+    </div>
+
+    <input type="radio" name="a" id="cr-3">
+    <label for="cr-3" style="--hue: 40"></label>
+    <div class="ci" style="--z: 2">
+        <h2 class="ch" style="--h: 40; --s: 100%; --l: 89%">In the mountains?</h2>
+        <img class="img" src="https://images.unsplash.com/photo-1518805629729-3e55b81b2393?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=700" alt="Mountains and houses">
+    </div>
+
+    <input type="radio" name="a" id="cr-4">
+    <label for="cr-4" style="--hue: 210"></label>
+    <div class="ci" style="--z: 1">
+        <h2 class="ch" style="--h: 210; --s: 70%; --l: 90%">Above looks beautiful?</h2>
+        <img class="img" src="https://images.unsplash.com/photo-1488831948116-38bc1351c6d6?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=700" alt="Sky and mountains">
+    </div>
+
+</div>
+<h1 class="new">NOUVEAUX ETABLISSEMENTS INSCRITS </h1>
+
+<ul class="list">
+    <li class="last1">$Variable Nom1</li>
+    <li class="last2">$Variable Nom2</li>
+    <li class="last3">$Variable Nom3</li>
+    <li class="last4">$Variable Nom4</li>
+    <li class="last5">$Variable Nom5</li>
+    <li class="last6">$Variable Nom6</li>
+</ul>
+
+
+<script src="../script/carousel.js"></script>
 <?php
 include '../template/footer.php';
 ?>
