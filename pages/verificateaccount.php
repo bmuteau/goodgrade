c49e59044d080c7a57ccb41d30469fc58d17b0b4
Brandon Muteau <muteaub@gmail.com>
2022-02-15 15:21:04 +0100

    verificate account page and responsive done

---
+++ pages/verificateaccount.php
@@ -0,0 +1,35 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="shortcut icon" type="image/png" href="../src/favicon/favicon-32x32.png" />
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap" rel="stylesheet">
+
+<link href="../styles/verificateaccount.css" rel="stylesheet">
+<link href="../styles/footer.css" rel="stylesheet">
+
+<body>
+
+    <img src="../src/Logo1.png" class="logo">
+    <p class="center">Merci de vous être inscrit sur GoodGrade.<br />
+
+        Vous avez reçu un email pour valider votre compte, veuillez cliquer dessus s'il vous plait. </p>
+
+
+</body>
+
+<footer>
+
+
+
+    <div class="footer">
+
+        <div class="faq"><a href="#">FAQ</a></div>
+        <a href="#top"> <img src="../src/iconfinder_arrow_forward_navigation_next_right_3994400.svg" class="up"></a>
+        <div class="contact"><a href="#">Nous contacter</a></div>
+        <div class="legal"><a href="#">Mentions légales</a></div>
+        <div class="copyright">GoodGrade, site d'avis sécurisé</div>
+
+    </div>
+</footer>
