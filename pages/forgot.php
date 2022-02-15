b76eedc328f09b28a17dbce0f62d6d566d6c82c7
Brandon Muteau <muteaub@gmail.com>
2022-02-15 19:34:27 +0100

    forgot password page and responsive done

---
+++ pages/forgot.php
@@ -0,0 +1,47 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="shortcut icon" type="image/png" href="../src/favicon/favicon-32x32.png" />
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap" rel="stylesheet">
+
+<link href="../styles/forgot.css" rel="stylesheet">
+<link href="../styles/footer.css" rel="stylesheet">
+
+<body>
+
+    <img src="../src/Logo1.png" class="logo">
+    <h1 class="title"> Mot de passe oublié ? </h1>
+
+    <div class="body">
+
+        <form method="post">
+            <div class="container">
+                <label for="email">Saisir votre adresse mail </label><br />
+                <input type="email" placeholder="Entrer votre adresse mail" name="email" required><br />
+                <button type="submit">Envoyer un mot de passe aléatoire</button>
+            </div>
+            <a href="login" class="log">Se connecter</a>
+
+    </div>
+
+    </form>
+    </div>
+
+</body>
+
+<footer>
+
+
+
+    <div class="footer">
+
+        <div class="faq"><a href="#">FAQ</a></div>
+        <a href="#top"> <img src="../src/iconfinder_arrow_forward_navigation_next_right_3994400.svg" class="up"></a>
+        <div class="contact"><a href="#">Nous contacter</a></div>
+        <div class="legal"><a href="#">Mentions légales</a></div>
+        <div class="copyright">GoodGrade, site d'avis sécurisé</div>
+
+    </div>
+</footer>
