e4c831dbd1a2f845b7f0bcc79538e0f8e24d0e6e
Brandon Muteau <muteaub@gmail.com>
2022-02-22 15:52:11 +0100

    paiementdone page and responsive done

---
+++ pages/paiementdone.php
@@ -0,0 +1,33 @@
+<?php
+include '../template/header.php';
+?>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link href="../styles/paiementdone.css" rel="stylesheet">
+<link href="../styles/footer.css" rel="stylesheet">
+
+<div id="top"></div>
+
+<body>
+    <h1 class="title">Merci pour votre commande ! <br />
+        Vous recevrez la facture et le suivi de votre commande par mail </h1>
+
+    <a href="../pages/home.php" class="backhome">RETOUR A L'ACCUEIL</a>
+    <!-- OU -->
+
+    <h1 class="title" style="visibility: hidden;">Une erreur est survenu, veuillez contacter le service client au 06 06 06 06 06 ou <br />
+        réessayer ulterieurement. </h1>
+
+</body>
+<footer>
+
+    <div class="footer">
+
+        <div class="faq"><a href="#">FAQ</a></div>
+        <a href="#top"> <img src="../src/iconfinder_arrow_forward_navigation_next_right_3994400.svg" class="up"></a>
+        <div class="contact"><a href="#">Nous contacter</a></div>
+        <div class="legal"><a href="#">Mentions légales</a></div>
+        <div class="copyright">GoodGrade, site d'avis sécurisé</div>
+
+    </div>
+</footer>
