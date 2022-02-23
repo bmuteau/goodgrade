864aad26f649ca1659f11cee274b686d026228b7
Brandon Muteau <muteaub@gmail.com>
2022-02-23 11:22:53 +0100

    qrcode page and responsive done

---
+++ pages/scannqrcode.php
@@ -0,0 +1,33 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="shortcut icon" type="image/png" href="../src/favicon/favicon-32x32.png" />
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap" rel="stylesheet">
+
+<link href="../styles/scannqrcode.css" rel="stylesheet">
+
+
+<body>
+
+    <a href="../pages/home.php"> <img src="../src/Logo1.png" class="logo"></a>
+
+    <p class="center">Déposer un avis </p>
+
+    <div class="wrapper">
+        <img src="../src/téléchargement (1).png" class="qrcode"> <!-- sera changer par le générateur -->
+        <div class="rules">
+            <ul>
+                <li>Scanne le QrCode</li>
+                <li>Connecte-toi à ton compte</li>
+                <li>Dépose l'avis maintenant ou plus tard ! </li>
+                <li> Merci </li>
+
+
+            </ul>
+        </div>
+    </div>
+
+
+</body>
